07246fc019254ac625cd5a744f1b55b74cdc446e
gt <jack139@gmail.com>
2021-01-06 16:11:14 +0800

    update

--- types/aminoreg.go
+++ types/aminoreg.go
@@ -3,8 +3,6 @@ package types
 import (
 	"crypto/elliptic"
 	"github.com/tendermint/go-amino"
-	//"github.com/tendermint/tendermint/crypto"
-	//"github.com/tendermint/tendermint/crypto/ed25519"
 	"github.com/tendermint/tendermint/types"
 	"github.com/tjfoc/gmsm/sm2"
 )
@@ -16,10 +14,6 @@ func init() {
 	AminoCdc.RegisterInterface((*IPayload)(nil), nil)
 	AminoCdc.RegisterConcrete(&Deal{}, "deal", nil)
 	AminoCdc.RegisterConcrete(&Auth{}, "auth", nil)
-	//AminoCdc.RegisterInterface((*crypto.PubKey)(nil), nil)
-	//AminoCdc.RegisterConcrete(ed25519.PubKey{}, "ed25519/pubkey", nil)
-	//AminoCdc.RegisterInterface((*crypto.PrivKey)(nil), nil)
-	//AminoCdc.RegisterConcrete(ed25519.PrivKey{}, "ed25519/privkey", nil)
 	AminoCdc.RegisterConcrete(sm2.PublicKey{}, "sm2/pubkey", nil)
 	AminoCdc.RegisterConcrete(sm2.PrivateKey{}, "sm2/privkey", nil)
 	AminoCdc.RegisterInterface((*elliptic.Curve)(nil), nil)
